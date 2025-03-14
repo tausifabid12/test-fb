3ecbf7104a8ccfe0623141f74308785da890ffc8
tausifabid12 <tausifabid1294@gmail.com>
2025-03-14 19:20:57 +0600

    dd

--- modules/endpoint/endpoint.route.js
+++ modules/endpoint/endpoint.route.js
@@ -4,9 +4,9 @@ const express_1 = require("express");
 const endpoint_controller_1 = require("./endpoint.controller");
 const router = (0, express_1.Router)();
 // Route for webhook verification
-// router.get('/facebook-webhook', verifyWebhook);
+router.get('/facebook-webhook', endpoint_controller_1.verifyWebhook);
 // Route for handling webhook events
-router.get('/facebook-webhook', endpoint_controller_1.handleWebhookData);
+router.post('/facebook-webhook', endpoint_controller_1.handleWebhookData);
 // // verification
 // router.get('/messenger-webhook', verifyMessengerWebhook);
 // //  events
